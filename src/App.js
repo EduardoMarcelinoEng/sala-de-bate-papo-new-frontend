0e85d34e0f66f61f343773dd12874379de6ebe4d
Eduardo Marcelino <eduardo.marcelino.eng@gmail.com>
2024-10-11 18:20:46 -0300

    Foram importados arquivos de estilos, store e rotas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { Provider } from 'react-redux';
 import './App.css';
+import Pages from './Pages';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NotificationContainer } from 'react-notifications';
+import 'react-notifications/lib/notifications.css';
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <NotificationContainer />
+        <Pages />
+      </div>
+    </Provider>
   );
 }
 
