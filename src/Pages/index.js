9fbcf508d456c58a20136b0a140ad821833eccc8
Eduardo Marcelino <eduardo.marcelino.eng@gmail.com>
2024-10-11 18:18:52 -0300

    Arquivo de rotas foi criado

---
+++ src/Pages/index.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    useNavigate
+} from "react-router-dom";
+import Load from '../Components/Load';
+import Layout from '../Components/Layout';
+import Login from './Login';
+import Chat from './Chat';
+import MyProfile from './MyProfile';
+import { useSelector } from 'react-redux';
+import PreConfig from "../Components/PreConfig";
+
+export default function Pages(){
+
+    const { isLoading } = useSelector(state => state.pageState);
+
+    return (
+        <BrowserRouter>
+            {
+                isLoading ? (
+                    <PreConfig>
+                        <Load />
+                    </PreConfig>
+                ) : (
+                    <Routes>
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/" element={<Layout />}>
+                            <Route path="meu-perfil" element={<MyProfile />} />
+                            <Route path="" element={<Chat />} />
+                        </Route>
+                    </Routes>
+                )
+            }
+        </BrowserRouter>
+    );
+}
